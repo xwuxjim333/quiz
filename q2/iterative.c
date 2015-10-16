4d40229e430ea13a466991b2e916132fb19c844f
xwuxjim333 <xwuxjim333@gmail.com>
2015-10-16 17:15:15 +0800

    q2: return the smallest character that is strictly larger than the search character.
    step 1: implement char smallest_character(char str[], char c)
    	both with iterative and recursive method.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,41 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <time.h>
+
+#define length 50
+
+char smallest_character(char str[], char c) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] > c) {
+            return str[i];
+        }
+        i++;
+    }
+
+//If no such character exists, return the smallest character in the array. 
+    return str[0];
+}
 
 int main()
 {
+    char str[length];
+    char c, out;
+    clock_t start, end;
+    double time;
+
+    printf("Input a sorted character array: ");
+    fgets(str, length, stdin);
+    printf("Input a character: ");
+    scanf("%c", &c);
+    fflush(stdin);
+
+    start = clock();
+    out = smallest_character(str, c);
+    end = clock();
+    time = (end - start) / (double)(CLOCKS_PER_SEC);
+
+    printf("Output: %c\n", out);
+    printf("execution time of smallest_character() : %lf sec\n", time);
     return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,48 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <time.h>
+
+#define length 50
+
+char smallest_character(char str[], char c)
+{
+    char out;
+    if (str[0] != '\0') {
+        if(str[0] > c) {
+            return str[0];
+        } else {
+            out = smallest_character(str+1, c);
+            if (out > c) {
+                return out;
+            } else {
+
+//If no such character exists, return the smallest character in the array. 
+                return str[0];
+            }
+        }
+    }
+    return 0;
+}
 
 int main()
 {
+    char str[length];
+    char c, out;
+    clock_t start, end;
+    double time;
+
+    printf("Input a sorted character array: ");
+    fgets(str, length, stdin);
+    printf("Input a character: ");
+    scanf("%c", &c);
+    fflush(stdin);
+
+    start = clock();
+    out = smallest_character(str, c);
+    end = clock();
+    time = (end - start) / (double)(CLOCKS_PER_SEC);
+
+    printf("Output: %c\n", out);
+    printf("execution time of smallest_character() : %lf sec\n", time);
     return 0;
 }
