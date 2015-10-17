5553d35780af374b6edbf1da5a1618430d2f2d06
swuxjim333 <xwuxjim333@gmail.com>
2015-10-17 22:14:32 +0800

    q4: maximum subarray problem
    step 1: implement int maxSubArry(int A[], int n)
    	both with iterative and recursive method

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,42 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <time.h>
+
+int maxSubArray(int A[], int n)
+{
+    int max = 0, test = 0;
+    int i = 0;
+    
+    for (i = 0; i < n; i++) {
+        test = test + A[i];
+        if (test <= 0) {
+            test = 0;
+        } else if (max < test) {
+            max = test;
+        }
+    }
+
+    return max;
+}
 
 int main()
 {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    double total_time;
+    clock_t start, end;
+    FILE *file = fopen("iterative.txt", "a");
+
+    start = clock();
+    int output = maxSubArray(arr, size);
+    end = clock();
+    printf("The largest sum: %d\n", output);
+
+    if (file) {
+	    total_time = (double)(end - start) / CLOCKS_PER_SEC;
+        fprintf(file, "%f\n", total_time);
+        fclose(file);
+    } 
+    printf("execution time of flatten() : %f sec\n", total_time);
+
     return 0;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,43 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <time.h>
+#define findmax(a, b) (((a) > (b)) ? (a) : (b))
+
+int maxSubArray(int A[], int n)
+{
+//Kadane's algorithm
+    int max = A[0], test = A[0];
+    int i = 0;
+
+    for (i = 0; i < n; i++) {
+        test = findmax(A[i], test+A[i]);
+        max = findmax(max, test);
+    }
+
+    if (max > 0)
+        return max;
+    else
+        return 0;
+}
 
 int main()
 {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    double total_time;
+    clock_t start, end;
+    FILE *file = fopen("recursive.txt", "a");
+
+    start = clock();
+    int output = maxSubArray(arr, size);
+    end = clock();
+    printf("The largest sum: %d\n", output);
+
+    if (file) {
+	    total_time = (double)(end - start) / CLOCKS_PER_SEC;
+        fprintf(file, "%f\n", total_time);
+        fclose(file);
+    } 
+    printf("execution time of flatten() : %f sec\n", total_time);
+
     return 0;
 }
